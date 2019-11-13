1968d6fef855aace95cbb7f19d5bf651c2f633d4
Apurva Rai <apurvarai99@gmail.com>
2019-11-13 10:44:42 -0600

    Added functions to Test.cpp and defined caller().

--- queue-starter/Test.cpp
+++ queue-starter/Test.cpp
@@ -5,3 +5,35 @@
 */
 
 #include "Test.h"
+
+void Test::isEmptyChecker()
+{}
+
+void Test::enqueueChecker()
+{}
+
+void Test::dequeueChecker()
+{}
+
+void Test::peekFrontChecker()
+{}
+
+void Test::constructorChecker()
+{}
+
+void Test::destructorChecker()
+{}
+
+void Test::caller()
+{
+
+isEmptyChecker();
+enqueueChecker();
+dequeueChecker();
+peekFrontChecker();
+constructorChecker();
+destructorChecker();
+
+}
+
+            
