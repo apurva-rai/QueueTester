5df13fb92d2cbea790b2184771cc33ae3a0efd10
Apurva Rai <apurvarai99@gmail.com>
2019-11-13 10:06:13 -0600

    First commit. Made Test header.

---
+++ queue-starter/Test.cpp
@@ -0,0 +1,7 @@
+/**
+*	@file Test.cpp
+*	@author Apurva Rai
+*	@date 201.11.13
+*/
+
+#include "Test.h"
---
+++ queue-starter/Test.h
@@ -0,0 +1,53 @@
+/**
+*	@file Test.h
+*	@author Apurva Rai
+*	@date 201.11.13
+*/
+
+#ifndef TEST_H
+#define TEST_H
+#include "Queue.h"
+
+class Test
+{
+
+  public:
+
+  /**
+  *	@post Outputs the test results of Queue method isEmpty()
+  */
+  void isEmptyChecker();
+
+  /**
+  *	@post Outputs the test results of Queue method enqueue()
+  */
+  void enqueueChecker();
+
+  /**
+  *	@post Outputs the test results of Queue method dequeue()
+  */
+  void dequeueChecker();
+
+  /**
+  *	@post Outputs the test results of Queue method peekFronty()
+  */
+  void peekFrontChecker();
+
+  /**
+  *	@post Outputs the test results of Queue method Queue()
+  */
+  void constructorChecker();
+
+  /**
+  *	@post Outputs the test results of Queue method ~Queue()
+  */
+  void destructorChecker();
+  
+  /**
+  *	@post Calls all the test methods
+  */
+  void caller();
+
+};
+
+#endif
